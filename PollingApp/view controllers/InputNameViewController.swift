2ed1c817df93533800ddb457fcf0933ad82add49
JonMercer <mercer.jon@gmail.com>
2016-02-07 20:42:29 -0800

    Ticket-01 Added assertion

--- PollingApp/view controllers/InputNameViewController.swift
+++ PollingApp/view controllers/InputNameViewController.swift
@@ -29,6 +29,7 @@ class InputNameViewController: UIViewController {
   
   func saveUser() {
     let userDefaults = NSUserDefaults.standardUserDefaults()
+    assert(nameTextField.text?.characters.count != 0 , "Name cannot be blank")
     let userName = nameTextField.text
     userDefaults.setValue(userName, forKey: UserDefaultKeys.userName)
     print(userName)
