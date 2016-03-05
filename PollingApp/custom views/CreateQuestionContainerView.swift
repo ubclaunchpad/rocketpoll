0d8bcab45c07a6584ccaccdaf0ff5b66ee578538
Mohamed Ali (laptop) <mohammedali.saisdubai@gmail.com>
2016-03-05 13:22:23 -0800

    IPA-15 (refactor)

--- PollingApp/custom views/CreateQuestionContainerView.swift
+++ PollingApp/custom views/CreateQuestionContainerView.swift
@@ -9,29 +9,11 @@
 import UIKit
 
 class CreateQuestionContainerView: UIView {
-    
-//    class func instanceFromNib(frame: CGRect) -> CreateQuestionContainerView {
-//        let view = UINib(nibName: "CreateQuestionContainerView", bundle: nil).instantiateWithOwner(nil, options: nil)[0] as! CreateQuestionContainerView
-//        view.frame = frame
-//        
-//        return view
-//    }
     @IBOutlet var view: UIView!
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         UINib(nibName: "CreateQuestionContainerView", bundle: nil).instantiateWithOwner(self, options: nil)
         addSubview(view)
         view.frame = self.bounds
-        
     }
-    
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
--- PollingApp/view controllers/CreateQuestionViewController.swift
+++ PollingApp/view controllers/CreateQuestionViewController.swift
@@ -8,22 +8,9 @@
 
 import UIKit
 
-// TODO: Mohammed is working here
-
 class CreateQuestionViewController: UIViewController {
-
-    
     var container: CreateQuestionContainerView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //setup()
     }
-    
-//    func setup() {
-//        // add your container class to view
-//        container = CreateQuestionContainerView.instanceFromNib(CGRectMake(0, 0, view.bounds.width, view.bounds.height))
-//        view.addSubview(container!)
-//    }
-
 }
