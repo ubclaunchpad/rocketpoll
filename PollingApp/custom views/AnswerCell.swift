6cac55b21867ee817d061add7c8c79c68d663161
Mohamed Ali (laptop) <mohammedali.saisdubai@gmail.com>
2016-03-05 13:35:42 -0800

    working on IPA-16

---
+++ PollingApp/custom views/AnswerCell.swift
@@ -0,0 +1,21 @@
+//
+//  AnswerCell.swift
+//  PollingApp
+//
+//  Created by Mohamed Ali on 2016-03-05.
+//  Copyright © 2016 Gabriel Uribe. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCell: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
