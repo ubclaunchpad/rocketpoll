dcdd73705671f959c34f89409d1b7538c6e26da3
Milton Leung <milleung964@gmail.com>
2016-03-16 22:03:20 -0700

    IPA-19 Setup

---
+++ PollingApp/view controllers/RoomsNearbyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RoomsNearbyViewController.swift
+//  PollingApp
+//
+//  Created by Milton Leung on 2016-03-15.
+//  Copyright © 2016 Gabriel Uribe. All rights reserved.
+//
+
+import UIKit
+
+class RoomsNearbyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
