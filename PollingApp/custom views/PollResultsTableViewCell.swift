d295059705cac8c28f64d7dbd20f2c877271375c
Cyrus Behroozi <cyrus.behr@gmail.com>
2016-03-24 15:33:43 -0700

    IPA-34 created poll results tableViewCell Nib and controller

---
+++ PollingApp/custom views/PollResultsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PollResultsTableViewCell.swift
+//  PollingApp
+//
+//  Created by Cyrus Behroozi on 2016-03-24.
+//  Copyright © 2016 Gabriel Uribe. All rights reserved.
+//
+
+import UIKit
+
+class PollResultsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
