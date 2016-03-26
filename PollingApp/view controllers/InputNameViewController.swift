77e7e64e7e765468621b5ae574856a0f8e62f3eb
Grace Lam <gracesmile88@hotmail.com>
2016-03-26 14:14:16 -0700

    Dismiss keyboard with enter key

--- PollingApp/view controllers/InputNameViewController.swift
+++ PollingApp/view controllers/InputNameViewController.swift
@@ -12,13 +12,20 @@ class InputNameViewController: UIViewController {
   @IBOutlet weak var nameTextField: UITextField!
   
   override func viewDidLoad() {
+    
     super.viewDidLoad()
+    nameTextField.delegate = self
+    
+
     let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
       action: "dismissKeyboard")
     view.addGestureRecognizer(tap)
   }
   
   @IBAction func submitButtonPressed(sender: AnyObject) {
+    
+    checkChars()
+    performSegueWithIdentifier(Segues.toMainApp, sender: self)
     
     //TODO:warn users to not use special characters instead of just stripping it
     let cleanedName = cleanName(nameTextField.text!)
@@ -57,6 +64,14 @@ class InputNameViewController: UIViewController {
 // MARK: - UITextFieldDelegate -
 extension InputNameViewController: UITextFieldDelegate {
   func textFieldShouldReturn(textField: UITextField) -> Bool {
+    
+    checkChars()
+    performSegueWithIdentifier(Segues.toMainApp, sender: self)
+    
+    return true
+    
+    
+    
     let cleanedName = cleanName(nameTextField.text!)
     if(cleanedName.characters.count <= 0) {
       let alert = UIAlertController(title: "Invalid name", message:"",
@@ -76,5 +91,16 @@ extension InputNameViewController: UITextFieldDelegate {
     
     return false
   }
+    
+    func checkChars() {
+        if(nameTextField.text?.characters.count == 0) {
+            print("DEBUG", "nameTextField.text = <empty>")
+            let alert = UIAlertController(title: "Please enter your name", message:"", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Got it", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
 }
 
