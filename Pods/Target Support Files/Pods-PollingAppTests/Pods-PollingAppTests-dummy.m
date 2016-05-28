cd9f20b5d05afc17eaeddca61490aaeae822e81f
Cyrus Behroozi <cyrus.behr@gmail.com>
2016-05-28 15:02:06 -0700

    removed outdated firebase

--- Pods/Target Support Files/Pods-PollingAppTests/Pods-PollingAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PollingAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_PollingAppTests
-@end
--- Pods/Target Support Files/Pods-PollingAppUITests/Pods-PollingAppUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_PollingAppUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PollingAppUITestsVersionString[];
-
