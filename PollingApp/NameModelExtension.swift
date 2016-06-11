e8426d8e7002a7dc412582d13c10614a918c6fef
Sherry Yuan <sherryuan@dhcp-128-189-250-158.ubcsecure.wireless.ubc.ca>
2016-06-11 12:27:52 -0700

    users can login to firebase

--- PollingApp/NameModelExtension.swift
+++ PollingApp/NameModelExtension.swift
@@ -7,11 +7,26 @@
 //
 
 import Foundation
+import Firebase
 
 extension ModelInterface: NameModelProtocol {
   
   // TODO: Write username to Firebase
   func setUserName(name: String) -> SegueName {
+    
+    let ref =  FIRDatabase.database().reference();
+
+    let udid = UIDevice.currentDevice().identifierForVendor!.UUIDString
+    
+    ref.queryEqualToValue(udid)
+    
+    let post = ["UserName": name,
+                "ListOfQuestion": "",
+                "QuestionsAnswered": ""];
+    let childUpdates = ["/Users/\(udid)": post]
+    
+    ref.updateChildValues(childUpdates)
+    
     return Segues.toMainApp
   }
 }
