9351237e3ee3efe59498ef44db116c5bacfa1a1f
James Park <QuantumSpark@dhcp-128-189-79-52.ubcsecure.wireless.ubc.ca>
2016-06-19 18:04:53 -0700

    IPA-86:Refactored PollResultsVC

--- PollingApp/view controllers/PollResultsViewController.swift
+++ PollingApp/view controllers/PollResultsViewController.swift
@@ -12,7 +12,6 @@ class PollResultsViewController: UIViewController {
     
     private var answerIDDictionary = [AnswerText: AnswerID]()
     private var questionID:QuestionID = ""
-    var container: PollResultsViewContainer?
     private var answers: [AnswerText] = []
     private var answerIDs: [AnswerID] = []
     private var correctAnswer: AnswerText = ""
@@ -20,6 +19,7 @@ class PollResultsViewController: UIViewController {
     private var NumResponsesPerAnswer: [Int] = []
     var totalNumberOfUserAnswers: Int = 0
     
+    var container: PollResultsViewContainer?
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -35,21 +35,7 @@ class PollResultsViewController: UIViewController {
         answerIDs = ModelInterface.sharedInstance.getSelectedQuestion().AIDS
         
         ModelInterface.sharedInstance.processAnswerData(answerIDs) { (listofAllAnswers) in
-            let size = listofAllAnswers.count
-            for i in 0 ..< size  {
-                let tempAnswer = listofAllAnswers[i].answerText
-                self.answerIDDictionary[tempAnswer] = self.answerIDs[i]
-                self.answers.append(tempAnswer)
-                if (listofAllAnswers[i].isCorrect == true ) {
-                    self.correctAnswer = listofAllAnswers[i].answerText
-                }
-            }
-            self.totalNumberOfUserAnswers = ModelInterface.sharedInstance.getSumOfUsersThatSubmittedAnswers(self.questionID)
-            
-            for i in 0...self.answerIDs.count-1{
-                self.NumResponsesPerAnswer.append(ModelInterface.sharedInstance.getNumberOfUsersThatGaveThisAnswer(self.questionID,answerID: self.answerIDs[i]))
-            }
-            
+            self.fillInTheFields(listofAllAnswers)
             self.container?.delegate = self
             self.container?.setTotalNumberOfAnswers(self.totalNumberOfUserAnswers)
             self.container?.setQuestionLabelText(questionText)
@@ -58,24 +44,25 @@ class PollResultsViewController: UIViewController {
             self.container?.setNumberOfResponsesForAnswer(self.NumResponsesPerAnswer)
             self.container?.resultsTableView.reloadData()
         }
-        
-        
-        
-        
     }
     
-//    func getAnswers(answerIDs: [AnswerID]) -> [Answer] {
-//        // Changes the list of answerIDs to list of answers
-//        var answers = [String]()
-//        var temp_answer:Answer
-//        
-//        for answerID in answerIDs {
-//            temp_answer = ModelInterface.sharedInstance.getAnswer(answerID)
-//            answers.append(temp_answer)
-//            answerIDDictionary[temp_answer] = answerID
-//        }
-//        return answers
-//    }
+    func fillInTheFields (listofAllAnswers: [Answer]) {
+        let size = listofAllAnswers.count
+        for i in 0 ..< size  {
+            let tempAnswer = listofAllAnswers[i].answerText
+            self.answerIDDictionary[tempAnswer] = self.answerIDs[i]
+            self.answers.append(tempAnswer)
+            if (listofAllAnswers[i].isCorrect == true ) {
+                self.correctAnswer = listofAllAnswers[i].answerText
+            }
+        }
+        self.totalNumberOfUserAnswers = ModelInterface.sharedInstance.getSumOfUsersThatSubmittedAnswers(self.questionID)
+        
+        for i in 0...self.answerIDs.count-1{
+            self.NumResponsesPerAnswer.append(ModelInterface.sharedInstance.getNumberOfUsersThatGaveThisAnswer(self.questionID,answerID: self.answerIDs[i]))
+        }
+    }
+    
 }
 
 
