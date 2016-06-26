ddf04aaecb37eb8ca2d7f92ad145b4c9beab8dd7
Milton <milleung964@gmail.com>
2016-06-26 01:56:02 -0700

    IPA-95 added back button in Poll Admin

--- PollingApp/view controllers/PollAdminViewController.swift
+++ PollingApp/view controllers/PollAdminViewController.swift
@@ -112,10 +112,12 @@ extension PollAdminViewController: PollAdminViewContainerDelegate {
         performSegueWithIdentifier(nextRoom, sender: self)
         print("SegueToResult");
     }
-    
+    func removeQuestion() {
+        ModelInterface.sharedInstance.removeQuestion(questionID)
+        segueToCampaign()
+    }
     func segueToCampaign() {
         let nextRoom =  ModelInterface.sharedInstance.segueToQuestionsScreen()
-        ModelInterface.sharedInstance.removeQuestion(questionID)
         performSegueWithIdentifier(nextRoom, sender: self)
         print("SegueToCampaign");
     }
