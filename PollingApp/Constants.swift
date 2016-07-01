dc3dff81ea3301f7bb46cd2117376d479c01c7ca
JonMercer <mercer.jon@gmail.com>
2016-07-01 15:18:43 -0700

    IPA-116 changed constants file

--- PollingApp/Constants.swift
+++ PollingApp/Constants.swift
@@ -8,35 +8,28 @@
 
 import Foundation
 
-
 typealias QuestionID = String
 typealias QuestionText = String
 typealias AnswerID = String
 typealias AnswerText = String
-
 typealias Author = String 
 typealias SegueName = String
 typealias RoomID = String
-var selectedQuestion = Question();
 
-var currentUser:Author = "";
+var selectedQuestion = Question(); //TODO: refactor this out of the Constants class
+
+var currentUser:Author = ""; //TODO: this should persist on phone restart
 
 var okayNameCharacters : Set<Character> =
-Set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890".characters)
-
-enum UserDefaultKeys {
-  static let userName = "username"
-}
+Set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890".characters)
 
 enum Segues {
   static let toMainApp = "showMainApp"
-  static let toSomeVC = "myIdentifier"
   static let toQuestionsScreen = "toQuestionsScreen"
   static let toPollUserViewController = "toPollUserViewController"
   static let toCreateQuestionView = "toCreateQuestionView"
   static let toPollAdminScreen = "toPollAdminScreen"
   static let toPollResultsView = "toPollResultsView"
   static let toPollAdminVCFromCampaign = "toPollAdminVCFromCampaign"
-    
 }
  
