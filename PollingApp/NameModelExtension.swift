17a49826d103d3b6cf1ab4a6cbffcc21b80c1467
Milton Leung <milleung964@gmail.com>
2016-07-02 18:00:03 -0700

    Change indentation

--- PollingApp/NameModelExtension.swift
+++ PollingApp/NameModelExtension.swift
@@ -10,41 +10,41 @@ import Foundation
 import Firebase
 
 extension ModelInterface: NameModelProtocol {
+  
+  // TODO: Check if udid exists
+  func setUserName(name: String) -> SegueName {
     
-    // TODO: Check if udid exists
-    func setUserName(name: String) -> SegueName {
-        
-        let ref =  FIRDatabase.database().reference();
-        
-        let udid = UIDevice.currentDevice().identifierForVendor!.UUIDString
-        
-        ref.queryEqualToValue(udid)
-        
-        let post = ["UserName": name,
-                    "ListOfQuestion": "",
-                    "QuestionsAnswered": ""];
-        let childUpdates = ["/Users/\(udid)": post]
-        
-        ref.updateChildValues(childUpdates)
-        
-        return Segues.toMainApp
+    let ref =  FIRDatabase.database().reference();
+    
+    let udid = UIDevice.currentDevice().identifierForVendor!.UUIDString
+    
+    ref.queryEqualToValue(udid)
+    
+    let post = ["UserName": name,
+                "ListOfQuestion": "",
+                "QuestionsAnswered": ""];
+    let childUpdates = ["/Users/\(udid)": post]
+    
+    ref.updateChildValues(childUpdates)
+    
+    return Segues.toMainApp
+  }
+  
+  func cleanName(name: String) -> String {
+    let strippedString = String(
+      name.characters.filter {okayNameCharacters.contains($0)})
+    let trimmedString = strippedString.stringByTrimmingCharactersInSet(
+      NSCharacterSet.whitespaceCharacterSet())
+    return trimmedString
+  }
+  
+  func isValidName(name:String) -> Bool {
+    for char in name.characters {
+      if (!okayNameCharacters.contains(char)) {
+        return false
+      }
     }
-    
-    func cleanName(name: String) -> String {
-        let strippedString = String(
-            name.characters.filter {okayNameCharacters.contains($0)})
-        let trimmedString = strippedString.stringByTrimmingCharactersInSet(
-            NSCharacterSet.whitespaceCharacterSet())
-        return trimmedString
-    }
-    
-    func isValidName(name:String) -> Bool {
-        for char in name.characters {
-            if (!okayNameCharacters.contains(char)) {
-                return false
-            }
-        }
-        return true
-    }
-    
+    return true
+  }
+  
 }
