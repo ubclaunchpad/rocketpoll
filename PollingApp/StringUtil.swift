d9812a3980ac12bf588875fb70e7e939fb8937ac
James Park <QuantumSpark@dhcp-128-189-75-91.ubcsecure.wireless.ubc.ca>
2016-07-30 16:15:06 -0700

    IPA-152: Can create a question through a unit test

--- PollingApp/StringUtil.swift
+++ PollingApp/StringUtil.swift
@@ -45,4 +45,20 @@ class StringUtil {
     }
     return texts.count == uniqueStrings.count
   }
+  
+  
+  static func randomStringWithLength (len : Int) -> NSString {
+    
+    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    
+    let randomString : NSMutableString = NSMutableString(capacity: len)
+    
+    for (var i=0; i < len; i++){
+      let length = UInt32 (letters.length)
+      let rand = arc4random_uniform(length)
+      randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+    }
+    
+    return randomString
+  }
 }
