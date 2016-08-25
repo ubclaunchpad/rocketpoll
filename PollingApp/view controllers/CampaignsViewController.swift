7289bccb0077bd3d44acf76abcdd070cc8302054
Milton <milleung964@gmail.com>
2016-08-25 12:03:52 -0700

    IPA-223 Fixed bug

--- PollingApp/view controllers/CampaignsViewController.swift
+++ PollingApp/view controllers/CampaignsViewController.swift
@@ -33,6 +33,10 @@ class CampaignsViewController: UIViewController {
     setNavigationBar()
   }
   
+  override func viewDidAppear(animated: Bool) {
+    refreshQuestions()
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
